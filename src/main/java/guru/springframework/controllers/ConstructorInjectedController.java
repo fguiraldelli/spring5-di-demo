67bc330411e822ae64a72086ddd5ab16754d4099
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-10 01:40:32 -0300

    Made exercise from manual-di to spring-di

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -2,14 +2,17 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    //here contructors is automatic @Autowired, so don't need this annotation here.
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
