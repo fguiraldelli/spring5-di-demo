d1b91eda866d69f5bf042573ab633bddc5bca84a
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-10 23:21:14 -0300

    Exercise di-assignment review to understand Life Bean Cycle

---
+++ src/main/java/guru/springframework/CustomBeanPostProcessor.java
@@ -0,0 +1,33 @@
+package guru.springframework;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by fguira on 2019-08-10 at 22:44
+ */
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+
+    }
+}
