3040e0648f671aae8567ebd588a16cceb29590ae
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-12 01:37:07 -0300

    Exercise from component-scan-example to factory-beans

---
+++ src/main/java/guru/springframework/services/GreetingServiceFactory.java
@@ -0,0 +1,26 @@
+package guru.springframework.services;
+
+/**
+ * Created by fguira on 2019-08-12 at 00:55
+ */
+public class GreetingServiceFactory {
+
+    private GreetingRepository greetingRepository;
+
+    public GreetingServiceFactory(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    public  GreetingService createGreetingService(String lang) {
+        switch (lang){
+            case "en":
+                return new PrimaryGreetingService(greetingRepository);
+            case "de":
+                return new PrimaryGermanGreetingService(greetingRepository);
+            case "es":
+                return new PrimarySpanishGreetingService(greetingRepository);
+            default:
+                return new PrimaryGreetingService(greetingRepository);
+        }
+    }
+}
