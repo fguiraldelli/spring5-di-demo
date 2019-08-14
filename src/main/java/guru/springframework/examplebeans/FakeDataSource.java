1837283444dd344b0f55340667a05d81e35c9a95
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-13 23:16:06 -0300

    Exercise Sec 94 - Property Source

---
+++ src/main/java/guru/springframework/examplebeans/FakeDataSource.java
@@ -0,0 +1,35 @@
+package guru.springframework.examplebeans;
+
+/**
+ * Created by fguira on 2019-08-13 at 23:02
+ */
+public class FakeDataSource {
+
+    private String user;
+    private String password;
+    private String url;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
